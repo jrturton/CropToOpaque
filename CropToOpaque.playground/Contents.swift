4ef3f51c67542f6b580f7226e4d6edee9532363f
richard.turton <jrturton@gmail.com>
2016-04-19 20:50:53 +0100

    Working

--- CropToOpaque.playground/Contents.swift
+++ CropToOpaque.playground/Contents.swift
@@ -15,25 +15,53 @@ UIGraphicsEndImageContext()
 let ciImage = CIImage(image: image)!
 
 let context = CIContext()
-let totalBytes = 4
-let bitmap = calloc(totalBytes, sizeof(UInt8))
-let colorSpace = CGColorSpaceCreateDeviceRGB()
 
 let maxAlpha = CIFilter(name: "CIAreaMaximumAlpha")!
 maxAlpha.setValue(ciImage, forKey:"inputImage")
 
-var topPoint: CGFloat = 0.0
-for topEdge in 0..<Int(ciImage.extent.size.height) {
-    maxAlpha.setValue(CIVector(CGRect:CGRect(x: 0, y: topEdge, width: Int(ciImage.extent.width), height: 1)), forKey: kCIInputExtentKey)
-    let maxAlphaImage = maxAlpha.outputImage!
-    context.render(maxAlphaImage, toBitmap: bitmap, rowBytes: totalBytes, bounds: maxAlphaImage.extent, format: kCIFormatRGBA8, colorSpace: colorSpace)
+let horizontalSlices = (0..<Int(ciImage.extent.size.height)).map {
+    return CGRect(x: 0, y: $0, width: Int(ciImage.extent.width), height: 1)
+}
+
+let verticalSlices = (0..<Int(ciImage.extent.size.width)).map {
+    return CGRect(x: $0, y: 0, width: 1, height: Int(ciImage.extent.height))
+}
+
+func alphaFromImage(image: CIImage, context: CIContext) -> Float {
+    let totalBytes = 4
+    let bitmap = calloc(totalBytes, sizeof(UInt8))
+    let colorSpace = CGColorSpaceCreateDeviceRGB()
+    
+    context.render(image, toBitmap: bitmap, rowBytes: totalBytes, bounds: CGRect(origin: .zero, size: CGSize(width:1, height: 1)), format: kCIFormatRGBA8, colorSpace: colorSpace)
     let rgba = UnsafeBufferPointer<UInt8>(
         start: UnsafePointer<UInt8>(bitmap),
         count: totalBytes)
     let alpha = Float(rgba[3]) / 255
-    if alpha > 0 {
-        topPoint = CGFloat(topEdge - 1)
-        break
-    }
+    return alpha
 }
+
+func firstNonAlphaSliceInImage(image: CIImage, slices: [CGRect]) -> CGFloat {
+    for (index, slice) in slices.enumerate() {
+        maxAlpha.setValue(CIVector(CGRect:slice), forKey: kCIInputExtentKey)
+        let maxAlphaImage = maxAlpha.outputImage!
+        let alpha = alphaFromImage(maxAlphaImage, context: context)
+        if alpha > 0 {
+            return CGFloat(index)
+        }
+    }
+    return 0
+}
+
+let topInset = firstNonAlphaSliceInImage(ciImage, slices: horizontalSlices)
+let bottomInset = firstNonAlphaSliceInImage(ciImage, slices: horizontalSlices.reverse())
+let leftInset = firstNonAlphaSliceInImage(ciImage, slices: verticalSlices)
+let rightInset = firstNonAlphaSliceInImage(ciImage, slices: verticalSlices.reverse())
+
+let cropInsets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+let croppedRect = UIEdgeInsetsInsetRect(ciImage.extent, cropInsets)
+
+let cropFilter = CIFilter(name: "CICrop", withInputParameters: [kCIInputImageKey : ciImage, "inputRectangle" : CIVector(CGRect:croppedRect)])
+let result = cropFilter?.outputImage!
+
+
 
